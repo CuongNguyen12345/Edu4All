454e3aeb4fb5251ee6663b94d0b0a265c2beaacc
unknown <chicuongnguyen193@gmail.com>
2025-11-15 15:39:20 +0700

    ok

--- app/src/main/java/com/example/myapplication/Activity/MainActivity.java
+++ app/src/main/java/com/example/myapplication/Activity/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         init();
+        UserDao dao = AppDatabase.getInstance(MainActivity.this).userDao();
 
         btnLogin.setOnClickListener(v -> {
             handleClickSignup();
